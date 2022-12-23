351403ea13cba066001c975d7d503c9cba22411f
Nigel Guven <nigelguven@gmail.com>
2022-12-23 17:23:20 +0000

    add template limit service from spring initializr

---
+++ java/limit-service/src/test/java/com/scion/currencyconverter/frsbrz/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scion.currencyconverter.frsbrz.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
