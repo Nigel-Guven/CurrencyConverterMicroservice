72b9ed2098706aa9cf1e69ff3361991371d17f79
Nigel Guven <nigelguven@gmail.com>
2022-12-24 01:15:21 +0000

    limit service bootstrapped to spring boot server

--- server/spring-cloud-config-server/src/main/java/com/scion/currencyconverter/frsbrz/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ server/spring-cloud-config-server/src/main/java/com/scion/currencyconverter/frsbrz/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,11 +2,15 @@ package com.scion.currencyconverter.frsbrz.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class SpringCloudConfigServerApplication {
+@EnableConfigServer
+public class SpringCloudConfigServerApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
 
